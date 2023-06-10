224a207a3be3fd5c8dff0c84a0aa6ee1088da0a3
vishvamjethlya <109756883+vishvamjethlya@users.noreply.github.com>
2023-06-10 09:25:17 +0530

    Added solution - LeetHub

--- Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
+++ Top View of Binary Tree - GFG/top-view-of-binary-tree.cpp
@@ -106,34 +106,30 @@ class Solution
     {
         
         vector<int> ans;
-        if(root == NULL) 
-        {
-            return ans;
-        }
         
-        map<int,int> topNode;
+        if(root == NULL) return ans;
+        
+        map <int, int> topNode;
         queue<pair<Node*, int> > q;
         
         q.push(make_pair(root, 0));
         
-        while(!q.empty()) {
+        while(!q.empty()){
             pair<Node*, int> temp = q.front();
             q.pop();
+            
             Node* frontNode = temp.first;
             int hd = temp.second;
             
-            //if one value is present for a HD, then do nothing
-            if(topNode.find(hd) == topNode.end())
+            //map m HD ki entry check kro..agr nhi milti to map m nayi entry banake use store krlo
+            if(!topNode[hd]) // iska mtlb entry nhi mili
                 topNode[hd] = frontNode -> data;
-                
-            if(frontNode->left)
-                q.push(make_pair(frontNode->left, hd-1));
-            if(frontNode->right)
-                q.push(make_pair(frontNode->right, hd+1));
+            if(frontNode -> left)
+                q.push(make_pair(frontNode->left, hd - 1));
+            if(frontNode -> right)
+                q.push(make_pair(frontNode->right, hd + 1));
         }
-        
-        for(auto i:topNode) 
-        {
+        for(auto i : topNode){
             ans.push_back(i.second);
         }
         return ans;
