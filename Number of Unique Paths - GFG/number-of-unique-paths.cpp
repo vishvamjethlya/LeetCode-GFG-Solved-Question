9d94c8c73e4d4b51b9f784e84221f5fe2863b2ce
vishvamjethlya <109756883+vishvamjethlya@users.noreply.github.com>
2023-09-23 16:45:34 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -8,34 +8,34 @@ using namespace std;
 
 class Solution
 {
-    public:
-        int solve(int i, int j, vector<vector<int>> &dp){
-            if(i == 0 && j == 0) return 1;
-            if(i < 0 || j < 0) return 0;
-            if(dp[i][j] != -1) return dp[i][j];
+    // public:
+    //     int solve(int i, int j, vector<vector<int>> &dp){
+    //         if(i == 0 && j == 0) return 1;
+    //         if(i < 0 || j < 0) return 0;
+    //         if(dp[i][j] != -1) return dp[i][j];
             
-            int up = solve(i-1, j, dp);
-            int left = solve(i, j-1, dp);
-            return dp[i][j] = (up+left);
-        }
+    //         int up = solve(i-1, j, dp);
+    //         int left = solve(i, j-1, dp);
+    //         return dp[i][j] = (up+left);
+    //     }
     public:
     //Function to find total number of unique paths.
     int NumberOfPath(int a, int b)
     {
-        vector<vector<int>> dp(a, vector<int>(b, -1));
-        // for(int i = 0; i < a, i++){
-        //     for(int j = 0; j < b, j++){
-        //         if(i == 0 && j == 0) dp[0][0] = 1;
-        //         else{
-        //             int up = 0;
-        //             if(i > 0) up += dp[i-1][j];
-        //             int left = 0;
-        //             if(j > 0) left += dp[i][j-1];
-        //             return dp[i][j] = (up+left);
-        //         }
-        //     }
-        // }
-        return solve(a-1, b-1, dp);
+        vector<vector<int>> dp(a, vector<int>(b, 0));
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j < b; j++){
+                if(i == 0 && j == 0) dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i > 0) up = dp[i-1][j];
+                    if(j > 0) left = dp[i][j-1];
+                    dp[i][j] = (up+left);
+                }
+            }
+        }
+        return dp[a-1][b-1];
     }
 };
 
